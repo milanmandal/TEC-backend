c1cb3ef60ee20e295025ae1d2246f4f3a8d29ef8
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-09-08 14:41:21 +0530

    adding path

--- model/user.js
+++ model/user.js
@@ -5,6 +5,10 @@ var UserSchema=new mongoose.Schema({
         type:String,
         require:true,
     },
+    team:{
+        type:String,
+        require:true,
+    },
     password:{
         type:String,
         require:true,
@@ -14,22 +18,89 @@ var UserSchema=new mongoose.Schema({
         type:String,
         require:true,
     },
-    score:{
+    score1:{
         type:Number,
         default:0
     },
-    invest:{
+    score2:{
         type:Number,
         default:0,
+    },
+    score3:{
+        type:Number,
+        default:0
     },
     max:{
         type:Number,
         default:0,
     },
     page:{
-        type:Number,
-        default:1,
-    }
+        type:String,
+        default:'/',
+    },
+
+        q1:{
+            type:Number,
+            default:0,
+        },
+        q2:{
+            type:Number,
+            default:0,
+        },
+        q3:{
+            type:Number,
+            default:0,
+        },
+        q4:{
+            type:Number,
+            default:0,
+        },
+        q5:{
+            type:Number,
+            default:0,
+        },
+        q6:{
+            type:Number,
+            default:0,
+        },
+        q7:{
+            type:Number,
+            default:0,
+        },
+        q8:{
+            type:Number,
+            default:0,
+        },
+        q9:{
+            type:Number,
+            default:0,
+        },
+        q10:{
+            type:Number,
+            default:0,
+        },
+
+ 
+        company1:{
+            type:String,
+            default:'None',
+        },
+        invest1:{
+            type:Number,
+            default:0,
+        },
+        company2:{
+            type:String,
+            default:'None',
+        },
+        invest2:{
+            type:Number,
+            default:0,
+        },
+      
+
+
+
 
 })
 
--- routes/auth.js
+++ routes/auth.js
@@ -21,23 +21,55 @@ router.get("/:id", (req, res) => {
         .catch((err) => res.status(400).json("Error: " + err));
 });
 
-//POST USER SCORE
-router.route("/score/:id").post((req, res) => {
+
+
+
+
+
+
+
+//POST USER SCORE-ROUND1
+router.route("/score1/:id").post((req, res) => {
     User.findById(req.params.id)
         .then((points) => {
-            points.email = req.body.email;
-            points.password = req.body.password;
-            points.company = req.body.company;
-            points.token = req.body.token;
-            points.score = req.body.score;
-            points.invest = req.body.invest;
-            points.page = req.body.page;
-            points.max = req.body.max;
+           
+            points.score1 = req.body.score1;
             
-
             points
                 .save()
                 .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+
+//POST USER SCORE-ROUND3
+router.route("/score3/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.score3 = req.body.score3;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+
+//POST PATH OF THE USER
+router.route("/path/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((route) => {
+           
+            route.page = req.body.path;
+            
+            route
+                .save()
+                .then(() => res.json(`user updates - ` + path))
                 .catch((err) => res.status(400).json("Error: " + err));
         })
         .catch((err) => res.status(400).json("couldnt find: " + err));
@@ -50,7 +82,8 @@ router.post("/register", async (req, res) => {
 
     //check if the email already exists
     const emailExists = await User.findOne({ email: req.body.email });
-    if (emailExists) return res.status(400).send("Email already exists");
+    const teamExists = await User.findOne({ team: req.body.team });
+    if (emailExists || teamExists) return res.status(400).send("Email or team-email already exists");
 
     //HASH_PASSWORDS
 
@@ -59,6 +92,7 @@ router.post("/register", async (req, res) => {
 
     const user = new User({
         email: req.body.email,
+        team: req.body.team,
         password: hashpassword,
         company: req.body.company,
         token: jwt.sign({ company: req.body.company, email:req.body.email }, process.env.SECRET),
@@ -77,16 +111,50 @@ router.post("/login", async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     const user = await User.findOne({ email: req.body.email });
-    const pass = await bcrypt.compare(req.body.password, user.password);
-    if (!user || !pass) {
-        return res.status(400).send("invalid credentials");
-    } else {
+    const team = await User.findOne({ team: req.body.email });
+    
+    if (user) {
         
-        //GETTING WEB-TOKENS
+        const userpass = await bcrypt.compare(req.body.password, user.password);
+        if(userpass)
+        {
+            const token = jwt.verify(user.token, process.env.SECRET);
+            console.log(token,".....MASTER-USER")
+            res.json(user);
+        }
+        else{
+            return res.status(400).send("invalid credentials");
+        }
+    
+    } 
+    else {
+        if (team) {
         
-        const token = jwt.verify(user.token, process.env.SECRET);
-        console.log(token)
-        res.json(user);
+            const teampass = await bcrypt.compare(req.body.password, team.password);
+            if(teampass)
+            {
+                const token = jwt.verify(team.token, process.env.SECRET);
+                console.log("TEAM-MEMBER")
+                const member={
+                    value:1,
+                    company:team.company,
+                    token:team.token,
+                    id:team._id,
+                    page:team.page,
+                }
+                console.log(member);
+                res.json(member);
+            }
+            else{
+                return res.status(400).send("invalid credentials");
+            }
+        
+        } 
+        else{
+            return res.status(400).send("invalid credentials");
+        }
+        
+       
     }
 });
 
