bfb0b107efa54e046c5b8ac31367d5b0bb5f1571
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-09-21 15:40:53 +0530

    added routes

--- routes/auth.js
+++ routes/auth.js
@@ -21,14 +21,193 @@ router.get("/:id", (req, res) => {
         .catch((err) => res.status(400).json("Error: " + err));
 });
 
+/****************************************************************************/
+/****************************************************************************/
+//CHECK FOR THE QUESTIONS ANSWERED IN EACH ROUND
+/****************************************************************************/
+/****************************************************************************/
+
+
+router.route("/question/1/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q1 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/2/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q2 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/3/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q3 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/4/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q4 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/5/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q5 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/6/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.sq6 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/7/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q7 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/8/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q8 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/9/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q9 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+router.route("/question/10/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q10 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+/**************************************************************************/
+/**************************************************************************/
+/**************************************************************************/
 
 
 
 
 
+/****************************************************************************/
+/****************************************************************************/
+//SETTING ORIGINAL STATE OF THE QUESTION IN EVERY COMPREHENSION ROUND
+/****************************************************************************/
+/****************************************************************************/
+
+router.route("/reset/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((question) => {
+           
+            question.q1 = req.body.flag;
+            question.q2 = req.body.flag;
+            question.q3 = req.body.flag;
+            question.q4 = req.body.flag;
+            question.q5 = req.body.flag;
+            question.q6 = req.body.flag;
+            question.q7 = req.body.flag;
+            question.q8 = req.body.flag;
+            question.q9 = req.body.flag;
+            question.q10 = req.body.flag;
+            
+            question
+                .save()
+                .then(() => res.json(`user updates - ` + question))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
 
 
+
+
+/************************************************************************/
+/************************************************************************/
 //POST USER SCORE-ROUND1
+/************************************************************************/
+/************************************************************************/
 router.route("/score1/:id").post((req, res) => {
     User.findById(req.params.id)
         .then((points) => {
@@ -43,8 +222,108 @@ router.route("/score1/:id").post((req, res) => {
         .catch((err) => res.status(400).json("couldnt find: " + err));
 });
 
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
 
+
+
+/************************************************************************/
+/************************************************************************/
+//POST USER SCORE-ROUND2 COMPANY NAME 1 & 2 INVESTMENT 1 & 2
+/************************************************************************/
+/************************************************************************/
+
+
+//COMPANY 1
+router.route("/company1/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.company1 = req.body.companyName1;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+//COMPANY 2
+router.route("/company2/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.company2 = req.body.companyName2;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+//INVESTMENT 1
+router.route("/invest1/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.invest1 = req.body.invest1;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+
+//INVESTMENT 2
+router.route("/invest2/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.invest2 = req.body.invest2;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+
+// TOTAL SCORE OF ROUND 2
+router.route("/score2/:id").post((req, res) => {
+    User.findById(req.params.id)
+        .then((points) => {
+           
+            points.score2 = req.body.score1;
+            
+            points
+                .save()
+                .then(() => res.json(`user updates - ` + points))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("couldnt find: " + err));
+});
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
+
+
+
+
+
+
+/************************************************************************/
+/************************************************************************/
 //POST USER SCORE-ROUND3
+/************************************************************************/
+/************************************************************************/
 router.route("/score3/:id").post((req, res) => {
     User.findById(req.params.id)
         .then((points) => {
@@ -58,6 +337,12 @@ router.route("/score3/:id").post((req, res) => {
         })
         .catch((err) => res.status(400).json("couldnt find: " + err));
 });
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
+
+
+
 
 
 //POST PATH OF THE USER
